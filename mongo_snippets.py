6cc3738d88067f4f1d80db2191d21e7bf2e411d6
Tyler Brock <tyler.brock@gmail.com>
2012-08-23 23:34:42 -0400

    whatev

--- mongo_snippets.py
+++ mongo_snippets.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import render_template
+
 mongo_snippets = Flask(__name__)
 
 @mongo_snippets.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
+    mongo_snippets.debug = True
     mongo_snippets.run()
